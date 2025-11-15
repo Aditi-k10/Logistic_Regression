b6151b7e2fb82524197c3b21f704872fa8427305
Aditi-k10 <khandeaditi@gmail.com>
2025-11-15 21:23:57 +0530

    app.py

--- app.py
+++ app.py
@@ -12,9 +12,15 @@ st.title("Logistic Regression Prediction App")
 feature1 = st.number_input("Enter value for Feature 1")
 feature2 = st.number_input("Enter value for Feature 2")
 feature3 = st.number_input("Enter value for Feature 3")
+feature4 = st.number_input("Enter value for Feature 4")
+feature5 = st.number_input("Enter value for Feature 5")
+feature6 = st.number_input("Enter value for Feature 6")
+feature7 = st.number_input("Enter value for Feature 7")
+feature8 = st.number_input("Enter value for Feature 8")
 
 # Combine inputs into a numpy array and scale
-input_data = np.array([[feature1, feature2, feature3]])
+input_data = np.array([[feature1, feature2, feature3, feature4,
+                        feature5, feature6, feature7, feature8]])
 input_data_scaled = scaler.transform(input_data)
 
 # Predict button
